fb8b07d46ff002004ea84726db191a80bf7833a0
Ryan Norris <ryan.norris@my.ccsu.edu>
2018-06-02 23:24:47 +0000

    website: Add privacy policy for GDPR compliance.
    Change-Id: I1293fce0cc02f08b1494292a04e0a89db8bb5b52

---
+++ assets/js/privacy.js
@@ -0,0 +1,12 @@
+<script>
+  (function () {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'https://app.termly.io/embed.min.js';
+    s.id = '7877de79-e324-4500-b796-52fb2f11c3f4';
+    s.setAttribute("data-name", "termly-embed-banner");
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+  })();
+</script>
