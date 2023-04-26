d50fb3e44ef3d14eadd97005808c1c0ac435a545
Christian Oder <myself5@carbonrom.org>
2023-04-26 22:39:41 +0200

    website: Remove Analytics
    This reverts commit 7fae7baab7c7d47146ef885a11158879e8f6e505.
    
    Change-Id: Ib2ebd0c062d956fd2de735ea6cd68ad253129814

--- changelog/index.php
+++ changelog/index.php
@@ -54,24 +54,6 @@
       z-index: 900;
     }
     </style>
-<!-- Site Analytics -->
-<script>
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-  _paq.push(["setCookieDomain", "*.carbonrom.org"]);
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//roxxonanalytics.com/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '3']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<noscript><p><img src="//roxxonanalytics.com/matomo.php?idsite=3&amp;rec=1" style="border:0;" alt="" /></p></noscript>
-<!-- End Site Analytics Code -->
   </head>
   <body>
     <div class="demo-layout mdl-layout mdl-layout--fixed-header mdl-js-layout mdl-color--grey-100">
